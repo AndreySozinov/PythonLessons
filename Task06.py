a2063dad7b87de946b900d1201a11d7e370dc396
AndreySozinov <savrey@ya.ru>
2022-04-16 08:42:08 +0500

    task 6 solved

--- Task06.py
+++ Task06.py
@@ -1,2 +1,8 @@
 # 6.	Дано число обозначающее день недели. Вывести его название 
 # и указать является ли он выходным.
+
+week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+        'Sunday']
+day_number = int(input('Введите номер дня недели\n'))
+print(week[day_number - 1])
+if day_number > 5: print('Это выходной день')
