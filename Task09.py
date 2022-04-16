216acce2b1c4b50ee01c929928706c77ff73b9c4
AndreySozinov <savrey@ya.ru>
2022-04-16 13:22:32 +0500

    task 9 solved

--- Task09.py
+++ Task09.py
@@ -1,2 +1,10 @@
 # 9.	Указав номер четверти прямоугольной системы координат, 
 # показать допустимые значения координат для точек этой четверти
+
+quarter_number = int(input('Введите номер четверти системы координат\n'))
+
+if quarter_number == 1: print('X > 0; Y > 0')
+elif quarter_number == 2: print('X < 0; Y > 0')
+elif quarter_number == 3: print('X < 0; Y < 0')
+elif quarter_number == 4: print('X > 0; Y < 0')
+else: print('Нет такой четверти')
