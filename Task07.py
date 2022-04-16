42ab7a0461e9131d1786db4d726a2cecf13a4aef
AndreySozinov <savrey@ya.ru>
2022-04-16 12:17:58 +0500

    task 7 solved

--- Task07.py
+++ Task07.py
@@ -1,2 +1,11 @@
 # 7.	Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для 
 # всех значений предикат
+
+predicats = [False, True]
+
+print('X      Y      Z      Проверка истинности')
+for x in predicats:
+    for y in predicats:
+        for z in predicats:
+            answer = not(x or y or z) is (not x and not y and not z)
+            print(x, y, z, '         ', answer)
