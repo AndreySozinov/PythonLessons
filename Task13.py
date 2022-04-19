fa9201d734c5fa55837a5970a652b5900017fe4f
AndreySozinov <savrey@ya.ru>
2022-04-19 19:08:28 +0500

    task 13 solved

--- Task13.py
+++ Task13.py
@@ -1,9 +1,15 @@
 # 13. Пользователь задаёт две строки. Определить количество вхождений 
 # одной строки в другой
 
-from itertools import count
 string_one = "Привет мир, привет страна!"
 string_two = "привет"
+sum = 0
 
-sum = count(string_two.islower() in string_one.islower())
+string_one = string_one.lower() # делаем все буквы маленькими
+string_two = string_two.lower()
+
+for i in range(0, len(string_one) - len(string_two) + 1):
+    if string_one[i:i+len(string_two)] == string_two:
+        sum += 1
+
 print(sum)
