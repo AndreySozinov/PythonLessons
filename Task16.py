6ccecf3540354101334385f80ab7634c021b04da
AndreySozinov <savrey@ya.ru>
2022-04-19 19:33:37 +0500

    task 16 solved

--- Task16.py
+++ Task16.py
@@ -1,2 +1,9 @@
 # 16. Написать программу проверки, является ли строка палиндромом
 
+palindrom = "а роза упала н алапу азор а"
+
+for i in range(0, int(len(palindrom) / 2)):
+    if palindrom[i] != palindrom[- 1 - i]:
+        print('Это не палиндром')
+        quit()
+print('Это палиндром')
