135aa639f64733a86794cdcc499f688f2a60f57e
AndreySozinov <savrey@ya.ru>
2022-04-25 21:13:50 +0500

    task22 solved

--- Task22.py
+++ Task22.py
@@ -1,1 +1,11 @@
 # Определить, присутствует ли в заданном списке строк, некоторое число
+
+list = ['adc21', 'i4op0', '33uio']
+
+number = 45
+
+for string in list:
+    if str(number) in string:
+        print('Присутствует')
+        quit()
+print("Отсутствует")
