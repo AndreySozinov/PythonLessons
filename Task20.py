7a0fa8572c6ac4cdf6c0b3c63ed4b7ecd45008b6
AndreySozinov <savrey@ya.ru>
2022-04-25 20:26:22 +0500

    task20 solved

--- Task20.py
+++ Task20.py
@@ -1,2 +1,13 @@
 # Задать список из n чисел последовательности (1+1/n)^n 
 # и вывести на экран их сумму
+
+n = int(input("Введите число N: "))
+
+list1 = []
+sum = 0
+
+for i in list(range(1, n + 1)):
+    list1.append((1 + 1 / i) ** i)
+    sum = sum + list1[i - 1]
+
+print(round(sum, 3))
