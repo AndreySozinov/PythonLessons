fb121776e3666d677d74ca6e501b1681e05e6516
AndreySozinov <savrey@ya.ru>
2022-05-14 09:27:33 +0500

    task 102 finished

--- Task102.py
+++ Task102.py
@@ -5,32 +5,37 @@
 from random import randint
 
 
+def PolynomForming (power, coefficients): # Формируем многочлен
+    polynomial = ""
+    for i in range(0, power - 1):
+        if coefficients[i] > 1:
+            polynomial = polynomial + f'{coefficients[i]}x^{power - i} + '
+        elif coefficients[i] == 1:
+            polynomial = polynomial + f'x^{power - i} + '
+    if coefficients[power - 1] > 1:
+        polynomial = polynomial + f'{coefficients[power - 1]}x'
+    elif coefficients[power - 1] == 1:
+        polynomial = polynomial + f'x'
+    else: polynomial = polynomial[:-3]
+    if coefficients[power] != 0:
+        polynomial = polynomial + f' + {coefficients[power]} = 0'
+    else:
+        polynomial = polynomial + ' = 0'
+    return polynomial
+
+
 k = int(input('k = ?\n'))
 
-polynomial1 = ""
-polynomial2 = ""
 
 coeff = [randint(0, 10) for x in range(k + 1)]
-#polynomial1 = "".join(f'{coeff[i]}x^{k - i} + ' for i in range(0, k - 1) if coeff[i] != 0) + f'{coeff[-2]}x + {coeff[-1]} = 0'
-for i in range(0, k - 1):
-    if coeff[i] > 1:
-        polynomial1 = polynomial1 + f'{coeff[i]}x^{k - i} + '
-    elif coeff[i] == 1:
-        polynomial1 = polynomial1 + f'x^{k - i} + '
-if coeff[k - 1] > 1:
-    polynomial1 = polynomial1 + f'{coeff[k - 1]}x'
-elif coeff[k - 1] == 1:
-    polynomial1 = polynomial1 + f'x'
-if coeff[k] != 0:
-    polynomial1 = polynomial1 + f' + {coeff[k]} = 0'
+polynom1 = (PolynomForming(k, coeff))
 
-print(polynomial1)
+coeff = [randint(0, 10) for x in range(k + 1)]
+polynom2 = (PolynomForming(k, coeff))
 
-# coeff = [randint(0, 10) for x in range(k + 1)]
-# #polynomial2 = "".join(f'{coeff[i]}x^{k - i} + ' for i in range(0, k - 1) if coeff[i] != 0) + f'{coeff[-2]}x + {coeff[-1]} = 0'
-# print (f'{polynomial1}\n{polynomial2}')
+print(f'{polynom1}\n{polynom2}')
 
-# file = open('file.txt', 'w')
-# file.write(f'{polynomial1}\n')
-# file.write(polynomial2)
-# file.close
+file = open('file.txt', 'w')
+file.write(f'{polynom1}\n')
+file.write(polynom2)
+file.close
