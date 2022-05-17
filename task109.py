02f5acb72a36285381f9619178ce1b7ca3af12d6
AndreySozinov <savrey@ya.ru>
2022-05-17 23:14:09 +0500

    task 109 began

--- task109.py
+++ task109.py
@@ -2,3 +2,16 @@
 # Используются операции +,-,/,*. приоритет операций стандартный. 
 # Пример: 2+2 => 4; 1+2*3 => 7; 1-2*3 => -5;
 
+expression = "11-2*300+6/2"
+
+# Разбиваем строку с выражением на числа и знаки операций
+elements = []
+pos = 0
+for i in range(len(expression)):
+    if not expression[i].isdigit(): 
+        elements.append(int(expression[pos:i]))
+        pos = i+1
+        elements.append(expression[i])
+elements.append(expression[pos:])
+
+print(elements)
